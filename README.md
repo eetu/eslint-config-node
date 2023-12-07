bbdf66581e63e98b30aedc3ecdb243785ce68095
Eetu Sutinen <eetu.sutinen@gmail.com>
2023-12-07 19:19:01 +0200

    Add base configuration

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# eslint_config_node
+# eslint-config-node
+
+ESlint configuration for node typescript projects
+
+## Install
+
+```bash
+yarn add -D eslint-config-node@https://github.com/eetu/eslint-config-node@1.0.0 eslint prettier typescript @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint-config-prettier
+```
---
+++ index.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "simple-import-sort", "unused-imports"],
+  rules: {
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
+  },
+};
